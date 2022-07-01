2122a4274de26e9e66f80994041cee99bb1b668a
Spencer Peace <peaces2@nku.edu>
2022-07-01 14:45:09 -0400

    Add generation of secondary isochrone from random point along first isochrone, add debug styling, add routing function

--- client/src/App.js
+++ client/src/App.js
@@ -15,10 +15,6 @@ mapboxgl.accessToken =
 
 // Test isochrone stuff
 const urlBase = "https://api.mapbox.com/isochrone/v1/mapbox/";
-const lon = -84.5;
-const lat = 39.1;
-const profile = "cycling"; // Set the default routing profile
-const meters = 1000; // Set the default distance
 
 export default class App extends React.PureComponent {
   constructor(props) {
@@ -27,7 +23,8 @@ export default class App extends React.PureComponent {
       lng: -84.5,
       lat: 39.1,
       zoom: 12,
-      distance: 10000, // meters by default
+      distance: 3, // miles by default, must be converted into meters
+      profile: "walking", // mode of transport for Isochrone, walking by default
       map: null,
       markerArr: [],
     };
@@ -51,11 +48,7 @@ export default class App extends React.PureComponent {
     // set the map state and subsequently set event listeners
     this.setState({ map: map }, () => {
       this.state.map.on("move", () => {
-        this.setState({
-          lng: map.getCenter().lng.toFixed(4),
-          lat: map.getCenter().lat.toFixed(4),
-          zoom: map.getZoom().toFixed(2),
-        });
+        // remove if unneeded
       });
 
       this.state.map.on("load", () => {
@@ -80,9 +73,16 @@ export default class App extends React.PureComponent {
       const newCoords = e.result.center;
       // Set new longitude/latitude and subsequently get isochrone
       this.setState({ lng: newCoords[0], lat: newCoords[1] }, () => {
-        this.getIso();
+        this.getIso().then(() => {
+          // get last marker and make an iso from it
+          const lastMarkerCoords =
+            this.state.markerArr[this.state.markerArr.length - 1].getLngLat();
+          this.getAndDrawNewIso(lastMarkerCoords);
+        });
       });
-      const marker = new mapboxgl.Marker().setLngLat(newCoords).addTo(map);
+      const marker = new mapboxgl.Marker({ color: "red" })
+        .setLngLat(newCoords)
+        .addTo(map);
       this.state.markerArr.push(marker);
       this.state.map.flyTo({
         center: newCoords,
@@ -98,16 +98,26 @@ export default class App extends React.PureComponent {
       this.state.markerArr.forEach((marker) => marker.remove());
       this.state.map.removeLayer("isoLayer");
       this.state.map.removeSource("iso");
+      this.state.map.removeLayer("newIsoLayer");
+      this.state.map.removeSource("newIso");
     });
   }
 
+  /**
+   * Must convert miles to meters before sending to API
+   */
   getIso = async () => {
     const query = await fetch(
-      `${urlBase}${profile}/${this.state.lng},${this.state.lat}?contours_meters=${this.state.distance}&polygons=true&access_token=${mapboxgl.accessToken}`,
+      `${urlBase}${this.state.profile}/${this.state.lng},${
+        this.state.lat
+      }?contours_meters=${Math.floor(
+        this.state.distance * 1609.34
+      )}&polygons=true&access_token=${mapboxgl.accessToken}`,
       { method: "GET" }
     );
     const data = await query.json();
     // Add isochrone source/layer if not already existing and set its' data
+    // the actual painting of the isochrone on the map can be removed in prod, this is simply for debugging purposes
     if (!this.state.map.getSource("iso")) {
       this.state.map.addSource("iso", {
         type: "geojson",
@@ -120,20 +130,110 @@ export default class App extends React.PureComponent {
       this.state.map.addLayer(
         {
           id: "isoLayer",
-          type: "fill",
+          type: "line",
           // Use "iso" as the data source for this layer
           source: "iso",
           layout: {},
           paint: {
-            // The fill color for the layer is set to a light purple
-            "fill-color": "#5a3fc0",
-            "fill-opacity": 0.3,
+            "line-color": "red",
           },
         },
         "poi-label"
       );
     }
     this.state.map.getSource("iso").setData(data);
+    const maxNumPoints = data.features[0].geometry.coordinates[0].length;
+    const randomPoint =
+      data.features[0].geometry.coordinates[0][
+        Math.floor(Math.random() * maxNumPoints /* (maxNumPoints / 3) */) // when uncommented, scopes random point to part of isochrone
+      ];
+    // testing! This grabs the northernmost point of the isochrone (approx.)
+    // const randomPoint = data.features[0].geometry.coordinates[0][0];
+    const marker = new mapboxgl.Marker({ color: "blue" })
+      .setLngLat(randomPoint)
+      .addTo(this.state.map);
+    this.state.markerArr.push(marker);
+    // get route from start to random point
+    // this.getRoute([this.state.lng, this.state.lat], randomPoint);
+    // this.getRoute(randomPoint, [this.state.lng, this.state.lat]);
+  };
+
+  // Draw new iso from last point
+  getAndDrawNewIso = async (newCoords) => {
+    const query = await fetch(
+      `${urlBase}${this.state.profile}/${newCoords.lng},${
+        newCoords.lat
+      }?contours_meters=${Math.floor(
+        this.state.distance * 1609.34
+      )}&polygons=true&access_token=${mapboxgl.accessToken}`,
+      { method: "GET" }
+    );
+    const data = await query.json();
+    if (!this.state.map.getSource("newIso")) {
+      this.state.map.addSource("newIso", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [],
+        },
+      });
+
+      this.state.map.addLayer(
+        {
+          id: "newIsoLayer",
+          type: "line",
+          // Use "iso" as the data source for this layer
+          source: "newIso",
+          layout: {},
+          paint: {
+            "line-color": "blue",
+          },
+        },
+        "poi-label"
+      );
+    }
+    this.state.map.getSource("newIso").setData(data);
+  };
+
+  /**
+   * Builds a route from start --> end and paints it on the map
+   * @param {start coordinate} start
+   * @param {end coordinate} end
+   */
+  getRoute = async (start, end) => {
+    const query = await fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+      { method: "GET" }
+    );
+
+    const json = await query.json();
+    const data = json.routes[0];
+    const route = data.geometry.coordinates;
+    const geojson = {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "LineString",
+        coordinates: route,
+      },
+    };
+    this.state.map.addLayer({
+      id: Math.random().toString(),
+      type: "line",
+      source: {
+        type: "geojson",
+        data: geojson,
+      },
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-color": "#3887be",
+        "line-width": 5,
+        "line-opacity": 0.75,
+      },
+    });
   };
 
   handleChange(event) {
@@ -152,17 +252,13 @@ export default class App extends React.PureComponent {
   }
 
   render() {
-    const { lng, lat, zoom } = this.state;
     return (
       <div>
-        <div className="sidebar">
-          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-        </div>
         <div ref={this.mapContainer} className="map-container" />
         <div id="geocoder"></div>
         <form onSubmit={this.handleSubmit}>
           <label>
-            Distance (meters):
+            Distance (miles):
             <br></br>
             <input
               type="text"
