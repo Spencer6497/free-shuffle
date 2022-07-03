bc7772c8c63322ba4e71a8b7ce989c393d460e06
Spencer Peace <peaces2@nku.edu>
2022-07-03 18:29:00 -0400

    Add form validation

--- client/src/Form/Form.js
+++ client/src/Form/Form.js
@@ -30,6 +30,8 @@ export default class Form extends React.Component {
         { name: "Cycling", value: "2" },
       ],
       radioButtonValue: "1",
+      validationMessage: "",
+      distanceValid: true,
       mapboxgl: props.mapboxgl,
       onGeocoderResult: props.onGeocoderResult,
       clearGeocoderResult: props.clearGeocoderResult,
@@ -49,26 +51,53 @@ export default class Form extends React.Component {
   }
 
   onSubmit(event) {
+    const form = event.currentTarget;
     event.preventDefault();
-    this.state.handleSubmit({
-      distance: this.state.distance,
-      distanceChanged: this.state.distanceChanged,
-      unit: this.state.unit,
-      mode: this.state.radioButtons
-        .filter(
-          (radioButton) => radioButton.value === this.state.radioButtonValue
-        )[0]
-        .name.toLowerCase(),
-    });
-    this.setState({ distanceChanged: false });
+    if (form.checkValidity() === true) {
+      this.state.handleSubmit({
+        distance: this.state.distance,
+        distanceChanged: this.state.distanceChanged,
+        unit: this.state.unit,
+        mode: this.state.radioButtons
+          .filter(
+            (radioButton) => radioButton.value === this.state.radioButtonValue
+          )[0]
+          .name.toLowerCase(),
+      });
+      this.setState({ distanceChanged: false });
+    }
   }
 
   onDistanceChanged(event) {
-    this.setState({ distance: event.target.value, distanceChanged: true });
+    this.setState(
+      { distance: event.target.value, distanceChanged: true },
+      () => {
+        this.distanceValid();
+      }
+    );
   }
 
   onUnitChanged(event) {
     this.setState({ unit: event.target.value.toLowerCase() });
+  }
+
+  distanceValid() {
+    if (!this.state.distance) {
+      this.setState({
+        validationMessage: "Distance is required.",
+        distanceValid: false,
+      });
+    } else {
+      const maxValue = this.state.unit === "mi" ? 62 : 99;
+      if (this.state.distance < 0.25 || this.state.distance > maxValue) {
+        this.setState({
+          validationMessage: `Please enter a value between 0.25 and ${maxValue}`,
+          distanceValid: false,
+        });
+      } else {
+        this.setState({ distanceValid: true });
+      }
+    }
   }
 
   render() {
@@ -81,17 +110,25 @@ export default class Form extends React.Component {
             clearGeocoderResult={this.state.clearGeocoderResult}
             initialCoords={this.state.initialCoords}
           ></Geocoder>
-          <FormComponent className="mb-3" onSubmit={this.onSubmit}>
+          <FormComponent noValidate className="mb-3" onSubmit={this.onSubmit}>
             <FormComponent.Group className="mb-3" controlId="distance">
               <FormComponent.Label>Distance:</FormComponent.Label>
               <Row className="distance-input">
                 <Col sm={8} md={10} className="mb-2">
                   <FormComponent.Control
-                    type="text"
+                    required
+                    type="number"
+                    min="0.25"
+                    max={this.state.unit === "mi" ? "62" : "99"}
+                    step="0.25"
                     placeholder="Desired distance"
+                    isInvalid={!this.state.distanceValid}
                     value={this.state.distance}
                     onChange={this.onDistanceChanged}
                   />
+                  <FormComponent.Control.Feedback type="invalid">
+                    {this.state.validationMessage}
+                  </FormComponent.Control.Feedback>
                 </Col>
                 <Col sm={4} md={2} className="mb-2">
                   <FormComponent.Select
